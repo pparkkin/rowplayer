887eab2a107701b1873820a3980e0d79a01ab487
Paavo <pparkkin@gmail.com>
2015-06-27 23:39:06 +0900

    Add decision problem and expected utility function.

--- src/Utility.hs
+++ src/Utility.hs
@@ -35,3 +35,35 @@ instance OrdPref Color where
     r Green Red = True
     r _ _ = True
 
+data OrdPref c => Decision s a c = Decision {
+    states :: [(s, Float)]
+  , actions :: [a]
+  , outcome :: s -> a -> c
+  }
+
+eu :: OrdPref c => a -> Decision s a c -> Float
+eu a (Decision ss _ o) = sum $ map eu' ss
+    where
+        eu' (s, p) = p * u' (o s a)
+        u' c = fromIntegral (u c)
+
+data Balls = Balls Color Color deriving (Show)
+
+bStates = [
+    (Balls Yellow Yellow, 0.05)
+  , (Balls Blue Yellow, 0.15)
+  , (Balls Red Yellow, 0.15)
+  , (Balls Green Red, 0.3)
+  , (Balls Red Blue, 0.3)
+  , (Balls Blue Blue, 0.05)
+  ]
+
+data Hands = LeftHand | RightHand deriving (Show)
+
+bActions = [LeftHand, RightHand]
+
+bOutcome (Balls l r) LeftHand = l
+bOutcome (Balls l r) RightHand = r
+
+bDecision = Decision bStates bActions bOutcome
+
