744822e0b19b2f9507bf12d509577599e2185fd2
Paavo <pparkkin@gmail.com>
2015-07-21 22:31:50 +0300

    Sample for buildGameTree.

--- src/Coalition.hs
+++ src/Coalition.hs
@@ -94,7 +94,7 @@ buildGameTree ss pof (p:ps) = let
             Just pid -> decision pid edges
             Nothing -> fail
 
-{-
+-- TEST
 data Party = Kesk | Peruss | Kok | SDP deriving (Show, Eq)
 type Portfolios = Int
 
@@ -103,54 +103,6 @@ seats = [(Kesk, 49)
        , (Peruss, 38)
        , (Kok, 37)
        , (SDP, 34)]
-
-seatsFor :: Seats Party -> Party -> NumSeats
-seatsFor ps p = fromMaybe 0 (lookup p ps)
-
-seatsTotal = 200 :: NumSeats
-portfoliosTotal = 24 :: Portfolios
-
-division :: Int -> Int -> Float
-division = (/) `on` fromIntegral
-
-shareOf = division
-
-seatsshares :: Seats Party -> [(Party, Float)]
-seatsshares = (`sharesOfTotal` seatsTotal)
-
-sharesOfTotal :: Seats Party -> NumSeats -> [(Party, Float)]
-sharesOfTotal ss total = map (second (`shareOf` total)) ss
-
-validcoalitions :: [(Party, Float)] -> [[Party]]
-validcoalitions ss = [c | c <- subsequences (parties ss), sharestotal ss c > 0.5]
-
-parties :: [(Party, a)] -> [Party]
-parties = map fst
-
-sharestotal :: [(Party, Float)] -> [Party] -> Float
-sharestotal ss ps = sum $ mapMaybe (`lookup` ss) ps
-
-portfolios :: Seats Party -> [(Party, Portfolios)]
-portfolios = (`splitPortfolios` portfoliosTotal)
-
-splitPortfolios :: Seats Party -> Portfolios -> [(Party, Portfolios)]
-splitPortfolios ss ps = zip (parties ss) (splitAmong ps (map snd ss))
-
-splitAmong :: Int -> [Int] -> [Int]
-splitAmong n ss = splitAmong' n ss'
-    where
-        ss' = map (`shareOf` t) ss
-        t = sum ss
-        splitAmong' l [s] = [l]
-        splitAmong' l (s:ss) = let k = n - r
-                                   r = round ((1 - s) * fromIntegral n) in
-            k : splitAmong' (l - k) ss
-
-coalitionSeats :: Seats Party -> [Party] -> Seats Party
-coalitionSeats ss = map (\p -> (p, seatsFor ss p))
-
-coalitionPortfolios :: Seats Party -> [Party] -> [(Party, Portfolios)]
-coalitionPortfolios ss ps = portfolios $ coalitionSeats ss ps
 
 distance :: Party -> Party -> Float
 distance p r = distance' p r / 4
@@ -170,20 +122,11 @@ distance' SDP Peruss = 26.82
 distance' SDP Kok = 13.96
 distance' _ _ = 0
 
-payoffs :: Party -> [Party] -> [(Party, Float)]
-payoffs f c = map (\p -> (p, payoff f p)) c
+payoffs :: [Party] -> Party -> [(Party, Float)]
+payoffs c f = map (\p -> (p, payoff f p)) c
     where
-        payoff f p = fromIntegral (portfolios p) - distance f p
-        portfolios p = fromMaybe 0 (lookup p (coalitionPortfolios seats c))
+        payoff f p = distance f p * (-1)
 
-turn :: [[Party]] -> Party -> IO ()
-turn cs' p = do
-    let cs = filter (elem p) cs'
-    print p
-    mapM_ (\c -> print $ payoffs p c) cs
+-- In REPL
+-- > buildGameTree seats payoffs (map fst seats)
 
-game :: [Party] -> IO ()
-game ps = do
-    let cs = validcoalitions $ seatsshares seats
-    mapM_ (turn cs) (reverse ps)
--}
